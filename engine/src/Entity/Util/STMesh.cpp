b8045a7ad9c363872624e0752d5c60c1aafe729b
Nathan Danque <nathan@danque.net>
2016-10-06 18:39:39 -0700

    Improved the parser for multiple OBJs [*] Adjusted so as the OBJ is validated upon import, its vertex data is also extracted avoid a 'second pass parsing'

--- engine/src/Entity/Util/STMesh.cpp
+++ engine/src/Entity/Util/STMesh.cpp
@@ -457,7 +457,176 @@ bool OBJMesh::Validate(const std::string &fileName, std::vector<std::string> *ta
 
 bool OBJMesh::Validate(const std::string &fileName, std::vector<std::string> *tags,
                        std::vector<STMesh_Structure> *dataMesh) {
-    return false;
+    //TODO - implement this.
+    stUint vertCount, texCount, normCount, objCount;
+    vertCount = texCount = normCount = objCount = 0;
+    std::string lastTag = "";
+    std::vector<Vector3<stUint>> vertCounts;
+    std::vector<Vector3<stReal>> _vertex;
+    std::vector<Vector2<stReal>> _texCoord;
+    std::vector<Vector3<stReal>> _normal;
+    std::vector<int> _index;
+
+    std::ifstream in(fileName.c_str(), std::ios_base::in);
+    if(!in){
+        std::cout << "Invalid file! Could not load: " << fileName << std::endl;
+        return false;
+    }
+    std::string line;
+    if(in.good()){
+        while(std::getline(in, line)){
+            //Vertex Extraction
+            if(line[0] == 'g'){
+                (*tags).push_back(line.substr(2));
+            }
+
+            if(line[0] == 'v' && line[1] == ' '){
+                if(lastTag == "f"){
+                    std::vector<int> adjustedIndicies;
+                    adjustedIndicies.reserve(_index.size());
+                    std::vector<Vertex> vertexList;
+                    std::vector<int> indexList;
+                    stUint ind = 0;
+                    if((*dataMesh).size() > 0){
+                        for(stUint i = 0, S = (stUint)_index.size(); i < S; i+= 3){
+                            adjustedIndicies[i] = _index.at(i) - vertCount;
+                            adjustedIndicies[i+1] = _index.at(i+1) - texCount;
+                            adjustedIndicies[i+2] = _index.at(i+2) - normCount;
+                        }
+                    } adjustedIndicies = _index;
+                    for(stUint i = 0, S = (stUint)adjustedIndicies.size(); i < S; i+= 3){
+                        Vertex vert(_vertex[adjustedIndicies.at(i)], _texCoord[adjustedIndicies.at(i+1), _normal[_index.at(i+2)]);
+                        vertexList.push_back(vert);
+                        indexList.push_back(ind);
+                        ind++;
+                    }
+                    STMesh_Structure mesh;
+                    mesh.m_vertices = vertexList;
+                    mesh.m_indices = indexList;
+                    (*dataMesh).push_back(mesh);//Mesh has been added to the list.
+
+                    _vertex.clear();
+                    _texCoord.clear();
+                    _normal.clear();
+                    _index.clear();
+                }
+                std::string vals = line.substr(2);
+                stReal _x = 0.0f, _y = 0.0f, _z = 0.0f;
+
+                std::string vX = vals.substr(0, vals.find(' '));
+                _x = (stReal)atof(vX.c_str());
+
+                std::string vY = vals.substr(vX.length()+1, vals.find(' '));
+                _y = (stReal)atof(vY.c_str());
+
+                std::string vZ = vals.substr(vals.find_last_of(' ')+1);
+                _z = (stReal)atof(vZ.c_str());
+
+                _vertex.push_back(Vector3<stReal>(_x, _y, _z));
+                vertCount++;
+                lastTag = "v";
+            }
+            //Texcoord Extraction
+            if(line[0] == 'v' && line[1] == 't' && line[2] == ' '){
+                std::string vals = line.substr(3);
+                stReal _u = 0.0f, _v = 0.0f;
+                std::string tU = vals.substr(0, vals.find(' '));
+                _u = (stReal)atof(tU.c_str());
+                std::string tV = vals.substr(tU.length()+1, vals.find(' '));
+                _v = (stReal)atof(tV.c_str());
+
+                _texCoord.push_back(Vector2<stReal>(_u, _v));
+                texCount++;
+            }
+            //Normal Extraction
+            if(line[0] == 'v' && line[1] == 'n'&& line[2] == ' '){
+                std::string vals = line.substr(3);
+                stReal _x = 0.0f, _y = 0.0f, _z = 0.0f;
+                std::string nX = vals.substr(0, vals.find(' '));
+
+                _x = (stReal)atof(nX.c_str());
+                std::string nY = vals.substr(nX.length()+1, vals.find(' '));
+
+                _y = (stReal)atof(nY.c_str());
+                std::string nZ = vals.substr(vals.find_last_of(' ')+1);
+
+                _z = (stReal)atof(nZ.c_str());
+
+                _normal.push_back(Vector3<stReal>(_x, _y, _z));
+                normCount++;
+            }
+
+            //Index Extraction
+            if(line[0] == 'f' && line[1] == ' '){
+                std::string hLine = line.substr(2);
+                int i = 0, j = 0, k = 0;
+                //first element
+                std::string face1 = hLine.substr(0, hLine.find(' '));
+                std::string v1 = face1.substr(0, face1.find('/'));
+                i = atoi(v1.c_str());
+                int midLen1 = face1.find_last_of('/') - face1.find('/')+1;
+                std::string t1 = face1.substr(face1.find('/')+1, midLen1);
+                j = atoi(t1.c_str());
+                std::string n1 = face1.substr(face1.find_last_of('/') + 1);
+                k = atoi(n1.c_str());
+                i--; j--; k--;
+                _index.push_back(i); _index.push_back(j); _index.push_back(k);
+                int midLen = hLine.find_last_of(' ') - hLine.find(' ') + 1;
+                std::string face2 = hLine.substr(hLine.find(' ') + 1, midLen);
+                std::string v2 = face2.substr(0, face2.find('/'));
+                i = atoi(v2.c_str());
+                int midLen2 = face2.find_last_of('/') - face2.find('/') + 1;
+                std::string t2 = face2.substr(v2.length()+1, midLen2);
+                j = atoi(t2.c_str());
+                std::string n2 = face2.substr(face2.find_last_of('/') + 1);
+                k = atoi(n2.c_str());
+                i--; j--; k--;
+                _index.push_back(i); _index.push_back(j); _index.push_back(k);
+                std::string face3 = hLine.substr(hLine.find_last_of(' ') + 1);
+                std::string v3 = face3.substr(0, face3.find('/'));
+                i = atoi(v3.c_str());
+                int midLen3 = face3.find_last_of('/')-face3.find('/') + 1;
+                std::string t3 = face3.substr(face3.find('/')+1, midLen3);
+                j = atoi(t3.c_str());
+                std::string n3 = face3.substr(face3.find_last_of('/')+1);
+                k = atoi(n3.c_str());
+                i--; j--; k--;
+                _index.push_back(i); _index.push_back(j); _index.push_back(k);
+            }
+        }
+        in.close();
+        if(lastTag == "f"){
+            std::vector<int> adjustedIndicies;
+            adjustedIndicies.reserve(_index.size());
+            std::vector<Vertex> vertexList;
+            std::vector<int> indexList;
+            stUint ind = 0;
+            if((*dataMesh).size() > 0){
+                for(stUint i = 0, S = (stUint)_index.size(); i < S; i+= 3){
+                    adjustedIndicies[i] = _index.at(i) - vertCount;
+                    adjustedIndicies[i+1] = _index.at(i+1) - texCount;
+                    adjustedIndicies[i+2] = _index.at(i+2) - normCount;
+                }
+            } adjustedIndicies = _index;
+            for(stUint i = 0, S = (stUint)adjustedIndicies.size(); i < S; i+= 3){
+                Vertex vert(_vertex[adjustedIndicies.at(i)], _texCoord[adjustedIndicies.at(i+1), _normal[_index.at(i+2)]);
+                vertexList.push_back(vert);
+                indexList.push_back(ind);
+                ind++;
+            }
+            STMesh_Structure mesh;
+            mesh.m_vertices = vertexList;
+            mesh.m_indices = indexList;
+            (*dataMesh).push_back(mesh);//Mesh has been added to the list.
+
+            _vertex.clear();
+            _texCoord.clear();
+            _normal.clear();
+            _index.clear();
+        }
+    }
+
+    return (*dataMesh).size() > 1;
 }
 
 

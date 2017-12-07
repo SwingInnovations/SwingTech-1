d0bc4e45dfab348aba8acdbc0fca10b33328bbec
Nathan Danque <nathan@danque.net>
2017-12-06 22:00:47 -0800

    [+] 3D Animation Component

--- engine/src/Entity/Components/ST3DAnimationComponent.cpp
+++ engine/src/Entity/Components/ST3DAnimationComponent.cpp
@@ -1,5 +1,15 @@
-//
-// Created by NDanq on 12/6/2017.
-//
-
 #include "ST3DAnimationComponent.h"
+
+
+void ST3DAnimationComponent::init(STEntity* parent) {
+    m_entity = parent;
+    m_gfxComponent = m_entity->get<STGraphicsComponent>();
+}
+
+ST3DAnimationComponent::ST3DAnimationComponent() {
+
+}
+
+ST3DAnimationComponent::ST3DAnimationComponent(const ST3DAnimationComponent &copy) {
+
+}

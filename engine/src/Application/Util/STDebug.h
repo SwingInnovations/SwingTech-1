3f5481c247defbb6f31848b0ab5f245f47d3cbaf
Nathan Danque <nathan@danque.net>
2018-03-15 14:50:49 -0700

    [+] Dev Vulkan Branch

---
+++ engine/src/Application/Util/STDebug.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#if _MSC_VER > 1900
+#include <Windows.h>
+#endif
+
+class STDebug {
+public:
+	static void Log(const std::string& output) {
+#if _MSC_VER > 1900
+		OutputDebugString((output + "\n").c_str());
+#else
+		std::cout << output << std::end;
+#endif
+	}
+
+	static void LogError(const std::string& output) {
+#if _MSC_VER > 1900
+		OutputDebugString((output + "\n").c_str());
+#else
+		std::cerr << output << std::endl;
+#endif
+	}
+};
